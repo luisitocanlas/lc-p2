63a0c8d9fd5a36e5f8452b21db2e85b702ebc7e7
Luisito Canlas <luisitocanlas@gmail.com>
2023-11-23 15:58:56 -0800

    update on switching sections

--- app.js
+++ app.js
@@ -4,15 +4,36 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
 function PageTransitions(){
-    // Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function() {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
-    }
+    let currentActiveButton;
 
+    // Button click active class
+    sectBtn.forEach(button => {
+        button.addEventListener('click', function() {
+            if (currentActiveButton) {
+                currentActiveButton.classList.remove('active-btn');
+            }
+            this.classList.add('active-btn');
+            currentActiveButton = this;
+        })
+    })
+
+    // Sections active using Event Delegation
+    document.body.addEventListener('click', (e) => {
+        const target = e.target;
+        const id = target.dataset.id;
+
+        if (id && target.classList.contains('control')) {
+            // Hide other sections
+            sections.forEach(section => section.classList.remove('active'));
+
+            // Add active class to the click section
+            const element = document.getElementById(id);
+            if (element) {
+                element.classList.add('active');
+            }
+        }
+    })
+    
 }
 
 PageTransitions();
