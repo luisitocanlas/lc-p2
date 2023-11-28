53d461a14a64b56203a4fda6e117a106c274b1b0
Luisito Canlas <luisitocanlas@gmail.com>
2023-11-27 20:37:20 -0800

    toggle color added

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ function PageTransitions(){
         }
     })
     
+    // Toggle Theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
